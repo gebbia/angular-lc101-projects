1f8463d72dbe693c45cb564c838e5e7a811fb601
Gebbia <gebbianatasha@gmail.com>
2022-07-18 06:15:18 -0500

    fav photos update

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Random Moon Knight Photos';
+  photosTitle = 'Moon Knight Photos';
   image1 = 'https://upload.wikimedia.org/wikipedia/en/f/fd/Marc_Spector_39.jpg';
   image2 = 'https://static.wikia.nocookie.net/disney/images/a/a6/Moon_Knight_Profile.jpg';
   image3 = 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/e/ea/Moon_Knight_Poster_Textless.png';
@@ -18,3 +18,4 @@ export class FavPhotosComponent implements OnInit {
   }
 
 }
+
